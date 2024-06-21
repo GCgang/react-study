1fd0c50bea2cdb024b8d5a3c81abd650eded88d3
GCgang <powm97@gmail.com>
2024-06-21 20:00:23 +0900

    Feat: useReducer  사용

--- basic/src/AppMentorsReducer.jsx
+++ basic/src/AppMentorsReducer.jsx
@@ -1,36 +1,22 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
+import personReducer from './reducer/person-reducer';
 
-export default function AppMentors() {
-  const [person, setPerson] = useState(initialPerson);
+export default function AppMentorsReducer() {
+  const [person, dispatch] = useReducer(personReducer, initialPerson);
+
   const handleUpdate = () => {
     const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
     const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.map((mentor) => {
-        if (mentor.name === prev) {
-          return {...mentor, name: current};
-        }
-        return mentor;
-      }),
-    }));
+    dispatch({type: 'updated', prev, current});
   };
   const handleAdd = () => {
     const name = prompt(`추가할 멘토의 name ?`);
     const title = prompt(`추가할 멘토의 title ?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: [
-        ...person.mentors, {id: Date.now(), name, title}
-      ]
-    }));
+    dispatch({type: 'added', name, title});
   };
   const handelDelete = () => {
     const name = prompt(`삭제할 멘토 name ?`);
-    setPerson((person) => ({
-      ...person,
-      mentors: person.mentors.filter((mentor) => mentor.name !== name)
-    }));
+    dispatch({type: 'deleted', name});
   };
   return (
     <div>
