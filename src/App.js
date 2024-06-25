8415cf03a567d644894637151be69cbfa1930e9c
GCgang <powm97@gmail.com>
2024-06-25 22:01:11 +0900

    Feat: layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import ToDoList from './components/ToDoList';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
     <div>
+      <Header />
+      <ToDoList />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/ToDoList.jsx
@@ -0,0 +1,5 @@
+export default function ToDoList() {
+    return (
+        <ul>To Do List</ul>
+    );
+}
