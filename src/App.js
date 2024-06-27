ab256ad64b7349314869f86e5652a5b5ff5c122e
GCgang <powm97@gmail.com>
2024-06-27 18:37:21 +0900

    Rename: 파일 이름 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import Header from './components/Header';
-import ToDoList from './components/ToDoList';
-import Footer from './components/Footer';
+import Header from './components/Header/Header';
+import ToDoList from './components/TodoList/TodoList';
+import AddTodo from './components/AddTodo/AddTodo';
 
 export default function App() {
   return (
     <div>
       <Header />
       <ToDoList />
-      <Footer />
+      <AddTodo />
     </div>
   );
 }
