319465dd5ef5268fbdd0169ccddfb2ba397c797a
GCgang <powm97@gmail.com>
2024-06-27 20:44:07 +0900

    Feat: Todo List 상태 업데이트 및 삭제

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,17 +1,27 @@
 import { useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
+import Todo from '../Todo/Todo';
 
 export default function ToDoList() {
-    const [todos, setTodos] = useState([]);
-    const handleAdd = (todo) => setTodos([...todos, todo]); // setTodos(prev => [...prev, todo])
-    return (
-        <section>
-            <ul>
-                {
-                    todos.map(todo => <li key={todo.id}>{todo.text}</li>)
-                }
-            </ul>
-            <AddTodo onAdd={handleAdd}/>
-        </section>
-    );
+  const [todos, setTodos] = useState([]);
+  const handleAdd = (todo) => setTodos([...todos, todo]);
+  const handleUpdate = (updated) =>
+    setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
+  const handleDelete = (deleted) =>
+    setTodos(todos.filter((t) => t.id !== deleted.id));
+  return (
+    <section>
+      <ul>
+        {todos.map((todo) => (
+          <Todo
+            id={todo.id}
+            todo={todo}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          />
+        ))}
+      </ul>
+      <AddTodo onAdd={handleAdd} />
+    </section>
+  );
 }
