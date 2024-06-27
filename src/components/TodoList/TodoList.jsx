13faaa3e14ce8ff15ac0727159b06780a5c89797
PEAKY BLINDERS <85366633+GCgang@users.noreply.github.com>
2024-06-28 02:35:34 +0900

    Feat: localstorage 사용하여 todo 기억하기

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,16 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 import styles from "./TodoList.module.css";
 
 export default function ToDoList({ filter }) {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(readTodosFromLocalStorage);
   const handleAdd = (todo) => setTodos([...todos, todo]);
   const handleUpdate = (updated) =>
     setTodos(todos.map((t) => (t.id === updated.id ? updated : t)));
   const handleDelete = (deleted) =>
     setTodos(todos.filter((t) => t.id !== deleted.id));
 
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   const filterd = getFilteredItems(todos, filter);
   return (
     <section className={styles.container}>
@@ -29,6 +32,11 @@ export default function ToDoList({ filter }) {
   );
 }
 
+function readTodosFromLocalStorage() {
+  console.log("read");
+  const todos = localStorage.getItem("todos");
+  return todos ? JSON.parse(todos) : [];
+}
 function getFilteredItems(todos, filter) {
   if (filter === "all") {
     return todos;
