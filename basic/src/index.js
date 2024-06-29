887d93e57adc77e4e08ddc1adb777f9c390619e2
GCgang <powm97@gmail.com>
2024-06-29 13:52:24 +0900

    Feat: 커스텀 훅 사용

--- basic/src/index.js
+++ basic/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import AppTheme from './AppTheme';
 import reportWebVitals from './reportWebVitals';
+import MainProducts from './components/MainProducts';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AppTheme />
+    <MainProducts />
   </React.StrictMode>
 );
 
