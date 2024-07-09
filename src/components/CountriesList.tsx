b671bdf5860cb35edbbee85842485bf6a294a0f8
GCgang <powm97@gmail.com>
2024-07-09 18:18:10 +0900

    Feat: CounteriesList

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -1,9 +1,18 @@
 import CountryListSection from './CountryListSection';
+import { eCountryStatus } from '../state/countiresState';
 
 export default function CountriesList() {
   return (
     <div>
-      <CountryListSection />
+      <CountryListSection title=" " status={eCountryStatus.TO_VISIT} />
+      <CountryListSection
+        title="내가 가본 나라들"
+        status={eCountryStatus.VISITED}
+      />
+      <CountryListSection
+        title="내가 좋아하는 나라들"
+        status={eCountryStatus.FAVORITE}
+      />
     </div>
   );
 }
